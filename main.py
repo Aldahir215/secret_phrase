2a4af604ae9ea75495b46ac35e8715a2be0f6d79
Aldahir <aldahir@protonmail.com>
2024-06-17 16:18:23 -0700

    Commiting to get the phrase

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("Give me the secret phrase!")
+print("Give me the second phrase!")
